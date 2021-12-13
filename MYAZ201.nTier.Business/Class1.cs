ac1eae563b556d2d7cc3404247ec03c31541a67d
sevval <sevval@DESKTOP-IKHJK1L>
2021-12-13 12:32:13 +0300

    Add project files.

---
+++ MYAZ201.nTier.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MYAZ201.nTier.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ MYAZ201.nTier.ConsoleApp.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MYAZ201.nTier.ConsoleApp.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
